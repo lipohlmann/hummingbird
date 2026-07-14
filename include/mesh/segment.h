1485423bf0296470a0369aa64146b109f4cd20c7
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-07-13 21:41:06 -0500

    start to fill in Segment header

--- include/mesh/segment.h
+++ include/mesh/segment.h
@@ -1,4 +1,15 @@
 #ifndef HUMMINGBIRD_MESH_SEGMENT_H
 #define HUMMINGBIRD_MESH_SEGMENT_H
 
-#endif // HUMMINGBIRD_MESH_SEGMENT_H
+#include <array>
+
+#include "mesh/element.h"
+namespace hummingbird::mesh {
+class Segment : public Element {
+ private:
+  std::array<uint32_t, 2>
+      node_indices;  // this might need to change for the quadrature stuff...
+};
+}  // namespace hummingbird::mesh
+
+#endif  // HUMMINGBIRD_MESH_SEGMENT_H
