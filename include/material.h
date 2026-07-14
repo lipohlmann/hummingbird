14927e64d39a6e1d804044f149e9c2de951eb49d
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-07-13 20:36:09 -0500

    create Material struct
    closes #3

---
+++ include/material.h
@@ -0,0 +1,29 @@
+#ifndef HUMMINGBIRD_MATERIAL_H
+#define HUMMINGBIRD_MATERIAL_H
+
+namespace hummingbird {
+
+/**
+ * @brief Defines a monoenergetic, constant-properties material
+ *
+ */
+struct Material {
+  /// @brief Total macroscopic cross section in 1/cm
+  double total_xs;
+
+  /// @brief Isotropic scattering macroscopic cross section in 1/cm
+  double scattering_xs;
+
+  /// @brief Fission macroscopic cross section in 1/cm
+  double fission_xs;
+
+  /// @brief Average number of neutrons produced per fission event
+  double neutrons_per_fission;
+
+  /// @brief Volumetric source in neutrons/cm3-s
+  double volumetric_source = 0;
+};
+
+}  // namespace hummingbird
+
+#endif  // HUMMINGBIRD_MATERIAL_H
