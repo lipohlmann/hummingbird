1d3d2791ec56ad09216179fe102933e6473bbad6
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-07-14 21:52:08 -0500

    fill in LegendrePolynomial function, need to write tests

--- include/math/legendre_polynomials.h
+++ include/math/legendre_polynomials.h
@@ -1,4 +1,18 @@
 #ifndef HUMMINGBIRD_MATH_LEGENDRE_POLYNOMIALS_H
 #define HUMMINGBIRD_MATH_LEGENDRE_POLYNOMIALS_H
 
-#endif // HUMMINGBIRD_MATH_LEGENDRE_POLYNOMIALS_H
+namespace hummingbird::math {
+/**
+ * @brief Computes the Legendre polynomial of degree n at a point x using the
+ * recursion formula. Legendre polynomials are defined on the interval \f$x\in
+ * [-1,1]\f$.
+ *
+ * @param n Order of polynomial to evaluate
+ * @param x Point to evaluate at
+ * @return double
+ */
+double LegendrePolynomial(const int n, const double x);
+
+}  // namespace hummingbird::math
+
+#endif  // HUMMINGBIRD_MATH_LEGENDRE_POLYNOMIALS_H
--- src/math/legendre_polynomials.cc
+++ src/math/legendre_polynomials.cc
@@ -1,2 +1,18 @@
 #include "math/legendre_polynomials.h"
 
+namespace hummingbird::math {
+double LegendrePolynomial(const int n, const double x) {
+  switch (n) {
+    case 0:
+      return 0;
+
+    case 1:
+      return x;
+
+    default:
+      return ((2.0 * n + 1.0) * x * LegendrePolynomial(n - 1, x) -
+              n * LegendrePolynomial(n - 2, x)) /
+             (n + 1.0);
+  }
+}
+}  // namespace hummingbird::math
