94fdb24958334dad2ee180e3b4f018e42a90310f
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-07-15 00:08:17 -0500

    add constants

---
+++ include/utils/constants.h
@@ -0,0 +1,11 @@
+#ifndef HUMMINGBIRD_UTILS_CONSTANTS_H_
+#define HUMMINGBIRD_UTILS_CONSTANTS_H_
+
+namespace hummingbird::utils {
+
+/// @brief General tolerance value for double comparisons
+constexpr double TOLERANCE = 1e-15;
+
+}  // namespace hummingbird::utils
+
+#endif  // HUMMINGBIRD_UTILS_CONSTANTS_H_
