807597bfd8fe38d02e27c0955c57a38c5f6febd8
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-07-15 17:17:16 -0500

    fill in ordinate class
    closes #6

--- include/ordinate.h
+++ include/ordinate.h
@@ -1,4 +1,52 @@
 #ifndef HUMMINGBIRD_ORDINATE_H
 #define HUMMINGBIRD_ORDINATE_H
 
-#endif // HUMMINGBIRD_ORDINATE_H
+namespace hummingbird {
+
+/**
+ * @brief Defines a solid angle, or *ordinate*, in spherical geometry
+ *
+ */
+class Ordinate {
+ public:
+  /**
+   * @brief Construct a new Ordinate object
+   *
+   * @param azimuth Azimuthal angle \f$\theta\in [-\pi,\pi]\f$
+   * @param polar Polar angle \f$\varphi\in [0,\pi]\f$
+   */
+  Ordinate(const double azimuth, const double polar);
+
+  /**
+   * @brief Evaluate the x-direction cosine
+   *
+   * @return double
+   */
+  double XCosine();
+
+  /**
+   * @brief Evaluate the y-direction cosine
+   *
+   * @return double
+   */
+  double YCosine();
+
+  /**
+   * @brief Evaluate the z-direction cosine
+   *
+   * @return double
+   */
+  double ZCosine();
+
+ private:
+  /// @brief Azimuthal angle in radians
+  const double azimuth_;
+
+  /// @brief Polar angle in radians
+  const double polar_;
+
+  void CheckInput();
+};
+}  // namespace hummingbird
+
+#endif  // HUMMINGBIRD_ORDINATE_H
--- src/ordinate.cc
+++ src/ordinate.cc
@@ -1,2 +1,26 @@
 #include "ordinate.h"
 
+#include <cmath>
+#include <stdexcept>
+
+namespace hummingbird {
+Ordinate::Ordinate(const double azimuth, const double polar)
+    : azimuth_(azimuth), polar_(polar) {
+  CheckInput(azimuth, polar);
+}
+
+void Ordinate::CheckInput(const double azimuth, const double polar) {
+  if (polar > M_PI) throw std::invalid_argument("Polar angle must be <= pi.");
+  if (polar < 0) throw std::invalid_argument("Polar angle must be >= 0.");
+  if (azimuth > M_PI)
+    throw std::invalid_argument("Azimuthal angle must be <= pi.");
+  if (azimuth < -M_PI)
+    throw std::invalid_argument("Azimuthal angle must be >= 0.");
+}
+
+double Ordinate::XCosine() { return std::cos(azimuth_) * std::sin(polar_); }
+
+double Ordinate::YCosine() { return std::sin(azimuth_) * std::sin(polar_); }
+
+double Ordinate::ZCosine() { return std::cos(polar_); }
+}  // namespace hummingbird
