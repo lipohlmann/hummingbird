44ab3ebcb76555819cb022d6dd724a9c293ac645
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-07-15 18:30:13 -0500

    start filling in, start changed to GLQuadrature

--- include/quadrature/gl_angular_quadrature.h
+++ include/quadrature/gl_angular_quadrature.h
@@ -1,4 +1,12 @@
-#ifndef HUMMINGBIRD_QUADRATURE_GL_ANGULAR_QUADRATURE_H
-#define HUMMINGBIRD_QUADRATURE_GL_ANGULAR_QUADRATURE_H
+#ifndef HUMMINGBIRD_QUADRATURE_GL_QUADRATURE_H
+#define HUMMINGBIRD_QUADRATURE_GL_QUADRATURE_H
 
-#endif // HUMMINGBIRD_QUADRATURE_GL_ANGULAR_QUADRATURE_H
+#include "quadrature/quadrature.h"
+namespace hummingbird::quadrature {
+class GLQuadrature : public Quadrature<double> {
+ public:
+  GLQuadrature(const unsigned int n_ordinates);
+};
+}  // namespace hummingbird::quadrature
+
+#endif  // HUMMINGBIRD_QUADRATURE_GL_ANGULAR_QUADRATURE_H
--- src/quadrature/gl_angular_quadrature.cc
+++ src/quadrature/gl_angular_quadrature.cc
@@ -1,2 +1,8 @@
 #include "quadrature/gl_angular_quadrature.h"
 
+#include "math/legendre_polynomials.h"
+
+namespace hummingbird::quadrature {
+GLQuadrature::GLQuadrature(const unsigned int n)
+    : Quadrature<double>(math::AllLegendreRoots(n)) {}
+}  // namespace hummingbird::quadrature
