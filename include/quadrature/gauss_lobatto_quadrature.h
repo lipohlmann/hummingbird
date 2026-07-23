4429cd621bd9c0644b669037a43142b87cbcd1d5
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-07-23 14:23:03 -0500

    Revert "start filling stuff"
    This reverts commit 6f33e6a50368b9e718d20e1d85f106425335ef1a.

--- include/quadrature/gauss_lobatto_quadrature.h
+++ include/quadrature/gauss_lobatto_quadrature.h
@@ -1,13 +1,4 @@
 #ifndef HUMMINGBIRD_QUADRATURE_GAUSS_LOBATTO_QUADRATURE_H_
 #define HUMMINGBIRD_QUADRATURE_GAUSS_LOBATTO_QUADRATURE_H_
 
-#include "quadrature/quadrature.h"
-
-namespace hummingbird::quadrature {
-class GaussLobattoQuadrature : public Quadrature<double> {
- public:
- private:
-};
-}  // namespace hummingbird::quadrature
-
-#endif  // HUMMINGBIRD_QUADRATURE_GAUSS_LOBATTO_QUADRATURE_H_
+#endif  // HUMMINGBIRD_QUADRATURE_GAUSS_LOBATTO_QUADRATURE_H
