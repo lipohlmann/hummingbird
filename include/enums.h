c4474b478fbe4a693f14848313d65c8c4ebc57f4
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-07-23 01:04:20 -0500

    add enums file, add boundaries

---
+++ include/enums.h
@@ -0,0 +1,8 @@
+#ifndef HUMMINGBIRD_ENUMS_H_
+#define HUMMINGBIRD_ENUMS_H_
+
+namespace hummingbird {
+enum class Boundary { TRANSMISSIVE, REFLECTIVE };
+}  // namespace hummingbird
+
+#endif  // HUMMINGBIRD_ENUMS_H_
