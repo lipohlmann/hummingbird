aaa5028fe94e733d8ae93ea63ca5dda4be02eb22
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-07-23 17:43:07 -0500

    define weight and abscissa methods, add docstrings

--- include/quadrature/gauss_chebyshev.h
+++ include/quadrature/gauss_chebyshev.h
@@ -1,4 +1,56 @@
 #ifndef HUMMINGBIRD_QUADRATURE_GAUSS_CHEBYSHEV_H_
 #define HUMMINGBIRD_QUADRATURE_GAUSS_CHEBYSHEV_H_
 
-#endif // HUMMINGBIRD_QUADRATURE_GAUSS_CHEBYSHEV_H_
+#include "quadrature/quadrature_base.h"
+
+namespace hummingbird::quadrature {
+/**
+ * @brief Class defines a Gauss-Chebyshev quadrature set, which approximates the
+ * integral of the form:
+ *
+ * \f[
+ * \int^{1}_{-1}\frac{u(x)}{\sqrt{1-x^2}}dx\approx \sum_{k=0}^{N-1}w_k u(\xi_k)
+ * \f]
+ *
+ */
+class GaussChebyshev : public QuadratureBase<double> {
+ public:
+  /**
+   * @brief Construct a new Gauss Chebyshev object
+   *
+   * @param n_points Total number of points desired
+   */
+  GaussChebyshev(const size_t n_points);
+
+ private:
+  /**
+   * @brief Computes the abscissa values for Gauss Chebyshev quadrature. These
+   * are given by the explicit formula:
+   *
+   * \f[
+   * \xi_k=\cos \left( \frac{(2k+1)\pi}{2N} \right)
+   * \f]
+   * for N total points.
+   *
+   * @param n_points
+   * @return std::vector<double>
+   */
+  std::vector<double> ComputeChebyshevAbscissas(const size_t n_points);
+
+  /**
+   * @brief Computes the quadrature weight for abscissa k of n total points. In
+   * Gauss-Chebyshev quadrature, all weights are equal and are given by:
+   *
+   * \f[
+   * w_k=\frac{\pi}{n}
+   * \f]
+   *
+   * @param k Abscissa number (not used in computation)
+   * @param n Total number of points
+   * @return Weight for abscissa k
+   */
+  double ComputeWeight(const size_t k, const size_t n) override;
+};
+}  // namespace hummingbird::quadrature
+
+#endif  // HUMMINGBIRD_QUADRATURE_GAUSS_CHEBYSHEV_H_
--- src/quadrature/gauss_chebyshev.cc
+++ src/quadrature/gauss_chebyshev.cc
@@ -1,2 +1,20 @@
 #include "quadrature/gauss_chebyshev.h"
 
+#include <cmath>
+namespace hummingbird::quadrature {
+GaussChebyshev::GaussChebyshev(const size_t n_points)
+    : QuadratureBase<double>(ComputeChebyshevAbscissas(n_points)) {}
+
+std::vector<double> GaussChebyshev::ComputeChebyshevAbscissas(
+    const size_t n_points) {
+  std::vector<double> abscissas(n_points);
+  for (auto i = 0; i < n_points; i++)
+    abscissas[i] = std::cos((2.0 * static_cast<double>(i) + 1.0) /
+                            (2.0 * static_cast<double>(n_points)) * M_PI);
+  return abscissas;
+}
+
+double GaussChebyshev::ComputeWeight(const size_t k, const size_t n) {
+  return M_PI / static_cast<double>(n);
+}
+}  // namespace hummingbird::quadrature
