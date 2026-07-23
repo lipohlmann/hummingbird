3b9cd7963d373a6c18ebca61db131dbbcd476359
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-07-23 15:05:51 -0500

    add abscissa calculation methods

--- include/quadrature/gauss_legendre_lobatto.h
+++ include/quadrature/gauss_legendre_lobatto.h
@@ -1,4 +1,31 @@
 #ifndef HUMMINGBIRD_QUADRATURE_GAUSS_LEGENDRE_LOBATTO_H_
 #define HUMMINGBIRD_QUADRATURE_GAUSS_LEGENDRE_LOBATTO_H_
 
-#endif // HUMMINGBIRD_QUADRATURE_GAUSS_LEGENDRE_LOBATTO_H_
+#include "quadrature/quadrature_base.h"
+
+namespace hummingbird::quadrature {
+class GaussLegendreLobatto : public QuadratureBase<double> {
+ public:
+  GaussLegendreLobatto(const size_t n_points);
+
+ private:
+  /**
+   * @brief Computes the abscissa values, given by:
+   *
+   * \f[
+   * \xi_k = \begin{cases}
+   * -1, & k=0\\
+   * \text{zeros of } P'_{N-1}, & 1\leq k\leq N-2\\
+   * 1,& k=N-1
+   * \end{cases}
+   * \f]
+   *
+   * @param n_points Total number of abscissa points
+   * @return std::vector<double>
+   */
+  std::vector<double> ComputeAbscissas(const size_t n_points);
+  double ComputeWeight(const size_t k, const size_t n) override;
+};
+}  // namespace hummingbird::quadrature
+
+#endif  // HUMMINGBIRD_QUADRATURE_GAUSS_LEGENDRE_LOBATTO_H_
--- src/quadrature/gauss_legendre_lobatto.cc
+++ src/quadrature/gauss_legendre_lobatto.cc
@@ -1,2 +1,21 @@
 #include "quadrature/gauss_legendre_lobatto.h"
 
+#include "math/legendre_polynomials.h"
+
+namespace hummingbird::quadrature {
+GaussLegendreLobatto::GaussLegendreLobatto(const size_t n_points)
+    : QuadratureBase<double>(ComputeAbscissas(n_points)) {}
+
+std::vector<double> GaussLegendreLobatto::ComputeAbscissas(
+    const size_t n_points) {
+  std::vector<double> abscissas(n_points);
+
+  abscissas[0] = -1.0;
+  abscissas.back() = 1.0;
+
+  auto legendre_derivative_roots = math::AllLegendrePrimeRoots(n_points - 1);
+  for (auto i = 1; i < abscissas.size() - 1; i++)
+    abscissas[i] = legendre_derivative_roots[i - 1];
+  return abscissas;
+}
+}  // namespace hummingbird::quadrature
