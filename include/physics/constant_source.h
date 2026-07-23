5e94dd73730cd0941cc44ecef80db9d383ce57d0
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-07-23 01:10:59 -0500

    constant source inherits from source base

--- include/physics/constant_source.h
+++ include/physics/constant_source.h
@@ -1,4 +1,10 @@
-#ifndef HUMMINGBIRD_PHYSICS_CONSTANT_SOURCE_H
-#define HUMMINGBIRD_PHYSICS_CONSTANT_SOURCE_H
+#ifndef HUMMINGBIRD_PHYSICS_CONSTANT_SOURCE_H_
+#define HUMMINGBIRD_PHYSICS_CONSTANT_SOURCE_H_
 
-#endif // HUMMINGBIRD_PHYSICS_CONSTANT_SOURCE_H
+#include "physics/source_base.h"
+
+namespace hummingbird::physics {
+class ConstantSource : public SourceBase {};
+}  // namespace hummingbird::physics
+
+#endif  // HUMMINGBIRD_PHYSICS_CONSTANT_SOURCE_H_
--- include/physics/source_base.h
+++ include/physics/source_base.h
@@ -1,4 +1,8 @@
 #ifndef HUMMINGBIRD_PHYSICS_SOURCE_BASE_H
 #define HUMMINGBIRD_PHYSICS_SOURCE_BASE_H
 
-#endif // HUMMINGBIRD_PHYSICS_SOURCE_BASE_H
+namespace hummingbird::physics {
+class SourceBase {};
+}  // namespace hummingbird::physics
+
+#endif  // HUMMINGBIRD_PHYSICS_SOURCE_BASE_H
