913f9f3ff1a4a88e211e00a571349f4a93a5def8
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-07-24 17:36:03 -0500

    fill in constructor

--- include/quadrature/angular/gauss_legendre_chebyshev.h
+++ include/quadrature/angular/gauss_legendre_chebyshev.h
@@ -1,4 +1,19 @@
 #ifndef HUMMINGBIRD_QUADRATURE_ANGULAR_GAUSS_LEGENDRE_CHEBYSHEV_H_
 #define HUMMINGBIRD_QUADRATURE_ANGULAR_GAUSS_LEGENDRE_CHEBYSHEV_H_
 
-#endif // HUMMINGBIRD_QUADRATURE_ANGULAR_GAUSS_LEGENDRE_CHEBYSHEV_H_
+#include "quadrature/angular/ordinate.h"
+#include "quadrature/gauss_chebyshev.h"
+#include "quadrature/gauss_legendre.h"
+#include "quadrature/quadrature_base.h"
+
+namespace hummingbird::quadrature::angular {
+class GaussLegendreChebyshev : public quadrature::QuadratureBase<Ordinate> {
+ public:
+  GaussLegendreChebyshev(const size_t n_half_azim, const size_t n_polar);
+
+ private:
+};
+
+}  // namespace hummingbird::quadrature::angular
+
+#endif  // HUMMINGBIRD_QUADRATURE_ANGULAR_GAUSS_LEGENDRE_CHEBYSHEV_H_
--- src/quadrature/angular/gauss_legendre_chebyshev.cc
+++ src/quadrature/angular/gauss_legendre_chebyshev.cc
@@ -1,2 +1,37 @@
 #include "quadrature/angular/gauss_legendre_chebyshev.h"
 
+#include <cmath>
+
+#include "quadrature/gauss_chebyshev.h"
+#include "quadrature/gauss_legendre.h"
+
+namespace hummingbird::quadrature::angular {
+
+GaussLegendreChebyshev::GaussLegendreChebyshev(const size_t n_half_azim,
+                                               const size_t n_polar) {
+  GaussChebyshev gc_azim_quad(n_half_azim);
+  GaussLegendre gl_polar_quad(n_polar);
+  size_t n_azim = 2 * n_half_azim;
+  size_t n_total = n_polar * n_azim;
+  abscissas_.reserve(n_total);
+
+  size_t counter = 0;
+  for (auto i = 0; i < n_polar; i++) {
+    for (auto j = 0; j < n_half_azim; j++) {
+      double azim = gc_azim_quad.GetAbscissa(j);
+      double polar = gl_polar_quad.GetAbscissa(i);
+
+      Ordinate positive(std::acos(azim), std::acos(polar));
+      abscissas_.push_back(std::move(positive));
+      double weight = GetWeight(i) * GetWeight(j);
+      weight_map_.insert({counter, weight});
+      counter++;
+
+      Ordinate negative(2.0 * M_PI - std::acos(azim), std::acos(polar));
+      abscissas_.push_back(std::move(negative));
+      weight_map_.insert({counter, weight});
+    }
+  }
+}
+
+}  // namespace hummingbird::quadrature::angular
