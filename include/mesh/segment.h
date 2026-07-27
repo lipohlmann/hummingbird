0200ec7cc18731841d51533cfd750ae2a6b95bb0
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-07-27 17:15:07 -0500

    add citation for Igel

--- include/mesh/segment.h
+++ include/mesh/segment.h
@@ -16,6 +16,25 @@ class Segment : public Element {
    */
   Segment(const std::array<size_t, 2> boundary_node_ids, const int material_id);
 
+  /**
+   * @brief Create interior Node objects using the Gauss-Lobatto-Legendre
+   * quadrature set. The mapping from the reference to the real domain was based
+   * on the equations provided by the book Computational Seismology by Heiner
+   * Igel
+   * (https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.geokniga.org/bookfiles/geokniga-computationalseismologyapracticalintroductionbyigelheinerz-liborg.pdf&ved=2ahUKEwiMuanq7_OVAxXVN4YAHVgAD1QQFnoECBUQAQ&usg=AOvVaw35g1Tl3ZSFUMusz8LKQxHk).
+   * For a line segment defined by two endpoints, the mapping is:
+   *
+   * \f[
+   * \mathbf{r}(\xi) = \mathbf{r}_1+\left( \frac{\xi+1}{2} \right)\left(
+   * \mathbf{r}_2-mathbf{r}_1 \right) \f]
+   *
+   * where \f$x\in[-1,1]\f$.
+   *
+   * @param existing_nodes Nodes already existing in Mesh
+   * @param gll_quadrature Gauss-Lobatto-Legendre quadrature set
+   * @return Additional nodes to be defined along the GLL quadrature set. Will
+   * be of length N-2 (the two endpoint account for the remaining two nodes)
+   */
   std::vector<Node> CreateInteriorNodes(
       const std::vector<Node>& existing_nodes,
       const quadrature::GaussLobattoLegendre& gll_quadrature) override;
