5c82c3d774a38b6ab60d13623a75a90c72aebe97
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-07-27 12:55:01 -0500

    add RenumberNodes method, add some members and a constructor

--- include/mesh/mesh.h
+++ include/mesh/mesh.h
@@ -1,4 +1,32 @@
 #ifndef HUMMINGBIRD_MESH_MESH_H_
 #define HUMMINGBIRD_MESH_MESH_H_
 
-#endif // HUMMINGBIRD_MESH_MESH_H_
+#include <vector>
+
+#include "mesh/element.h"
+#include "mesh/node.h"
+
+namespace hummingbird::mesh {
+
+/**
+ * @brief Class defing a mesh
+ *
+ */
+class Mesh {
+ public:
+  Mesh();
+
+ private:
+  std::vector<Node> nodes_;
+  std::vector<std::unique_ptr<Element>> elements_;
+
+  /**
+   * @brief Renumber nodes in mesh to keep node IDs near each other in a single
+   * element
+   *
+   */
+  void RenumberNodes();
+};
+}  // namespace hummingbird::mesh
+
+#endif  // HUMMINGBIRD_MESH_MESH_H_
--- src/mesh/mesh.cc
+++ src/mesh/mesh.cc
@@ -1,2 +1,23 @@
 #include "mesh/mesh.h"
 
+#include <algorithm>
+
+namespace hummingbird::mesh {
+void Mesh::RenumberNodes() {
+  std::sort(nodes_.begin(), nodes_.end(),
+            [](const Node& first, const Node& second) {
+              return first.id < second.id;
+            });
+
+  uint32_t new_node_id = 0;
+  for (auto& element : elements_) {
+    auto node_ids = element->node_ids();
+    for (auto id : node_ids) {
+      // Elements will share nodes. Don't renumber if we've already set a new ID
+      if (id < new_node_id) continue;
+      nodes_.at(id).id = new_node_id;
+      new_node_id++;
+    }
+  }
+}
+}  // namespace hummingbird::mesh
