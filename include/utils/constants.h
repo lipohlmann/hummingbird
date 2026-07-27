1059fb760b3463820fbeb2595337e9deb0072f67
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-07-27 10:57:10 -0500

    add EXP_NEAR_TOLERANCE for a more-relaxed comparison in tests

--- include/utils/constants.h
+++ include/utils/constants.h
@@ -6,6 +6,9 @@ namespace hummingbird::utils {
 /// @brief General tolerance value for double comparisons
 constexpr double TOLERANCE = 1e-15;
 
+/// @brief Tolerance value for EXPECT_NEAR in tests
+constexpr double EXP_NEAR_TOLERANCE = 1e-14;
+
 }  // namespace hummingbird::utils
 
 #endif  // HUMMINGBIRD_UTILS_CONSTANTS_H_
