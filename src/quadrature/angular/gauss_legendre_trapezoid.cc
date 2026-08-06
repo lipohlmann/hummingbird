8d63c38a5809c1cf3c7a1b8dd5325326bab9ca95
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-08-05 23:31:52 -0500

    fill in constructor

--- src/quadrature/angular/gauss_legendre_trapezoid.cc
+++ src/quadrature/angular/gauss_legendre_trapezoid.cc
@@ -1,2 +1,26 @@
 #include "quadrature/angular/gauss_legendre_trapezoid.h"
 
+#include <cmath>
+namespace hummingbird::quadrature::angular {
+GaussLegendreTrapezoid::GaussLegendreTrapezoid(const size_t n_azim,
+                                               const size_t n_polar) {
+  abscissas_.reserve(n_azim * n_polar);
+  GaussLegendre gl_polar_quad(n_polar);
+
+  double delta_azim = 2.0 * M_PI / (static_cast<double>(n_azim - 1));
+  size_t counter = 0;
+  for (auto i = 0; i < n_polar; i++) {
+    double polar = gl_polar_quad.GetAbscissa(i);
+    for (double j = 0; j < n_azim; j++) {
+      double azim = j * delta_azim;
+
+      Ordinate new_ordinate(std::acos(azim), std::acos(polar));
+      abscissas_.push_back(std::move(new_ordinate));
+
+      double weight = gl_polar_quad.GetWeight(i) * delta_azim;
+      weight_map_.insert({counter, weight});
+      counter++;
+    }
+  }
+}
+}  // namespace hummingbird::quadrature::angular
