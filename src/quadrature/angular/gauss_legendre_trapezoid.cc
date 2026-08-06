bc7b84426e82473b2c6fbcfb26abdcc9ff0cb9db
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-08-05 23:48:29 -0500

    fix starting value for azimuthal, do not compute arccos since we are directly computing the azimuthal angle

--- src/quadrature/angular/gauss_legendre_trapezoid.cc
+++ src/quadrature/angular/gauss_legendre_trapezoid.cc
@@ -7,14 +7,14 @@ GaussLegendreTrapezoid::GaussLegendreTrapezoid(const size_t n_azim,
   abscissas_.reserve(n_azim * n_polar);
   GaussLegendre gl_polar_quad(n_polar);
 
-  double delta_azim = 2.0 * M_PI / (static_cast<double>(n_azim - 1));
+  double delta_azim = 2.0 * M_PI / (static_cast<double>(n_azim));
   size_t counter = 0;
   for (auto i = 0; i < n_polar; i++) {
     double polar = gl_polar_quad.GetAbscissa(i);
     for (double j = 0; j < n_azim; j++) {
-      double azim = j * delta_azim;
+      double azim = -M_PI + j * delta_azim;
 
-      Ordinate new_ordinate(std::acos(azim), std::acos(polar));
+      Ordinate new_ordinate(azim, std::acos(polar));
       abscissas_.push_back(std::move(new_ordinate));
 
       double weight = gl_polar_quad.GetWeight(i) * delta_azim;
