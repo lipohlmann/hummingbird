490b656dba5f8c9bafc05d245e80e2e7cfa2ef5a
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-08-28 16:10:57 -0500

    update methods to match starling

--- include/utils/misc.h
+++ include/utils/misc.h
@@ -1,17 +1,36 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright (c) 2026, rcana-project
+// Copyright (c) 2026, Liam Pohlmann
+//
+// Portions of this file are derived from rcana-project and starling.
+
 #ifndef HUMMINGBIRD_UTILS_MISC_H_
 #define HUMMINGBIRD_UTILS_MISC_H_
+
+#include "utils/constants.h"
 
 namespace hummingbird::utils {
 /**
  * @brief Tests if two double-type numbers are equal using the utils::TOLERANCE
- * value. Taken from https://github.com/rcana-project/rcana/
+ * value. Taken from https://github.com/starling
  *
  * @param first First number to compare
  * @param second Second number to compare
  * @return true
  * @return false
  */
-bool DoubleEqual(const double first, const double second);
+bool DoubleEqual(const double first, const double second,
+                 const double tolerance = TOLERANCE);
+
+/**
+ * @brief Compute the relative error between two values
+ *
+ * @param new_val New value
+ * @param old_val Old value
+ * @return double
+ */
+double RelativeError(const double new_val, const double old_val);
 }  // namespace hummingbird::utils
 
 #endif  // HUMMINGBIRD_UTILS_MISC_H_
--- src/utils/misc.cc
+++ src/utils/misc.cc
@@ -5,8 +5,14 @@
 #include "utils/constants.h"
 
 namespace hummingbird::utils {
-bool DoubleEqual(const double first, const double second) {
+bool DoubleEqual(const double first, const double second,
+                 const double tolerance) {
   double difference = second - first;
-  return (std::abs(difference) < TOLERANCE);
+  return (std::abs(difference) < tolerance);
 }
+
+double RelativeError(const double new_val, const double old_val) {
+  return std::abs((new_val - old_val) / new_val);
+}
+
 }  // namespace hummingbird::utils
