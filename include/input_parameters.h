8383e9057d3a85056241c62bc13729055d23f6ab
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-09-03 17:26:58 -0500

    create input parameteres header

---
+++ include/input_parameters.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2026, Liam Pohlmann
+
+#ifndef HUMMINGBIRD_INPUT_PARAMETERS_H_
+#define HUMMINGBIRD_INPUT_PARAMETERS_H_
+
+namespace hummingbird {}  // namespace hummingbird
+
+#endif  // HUMMINGBIRD_INPUT_PARAMETERS_H_
