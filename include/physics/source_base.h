30cd0671873f98ef01eb4e6f9d7b4a4d3c1cd470
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-09-07 17:03:43 -0500

    recreate SourceBase class

---
+++ include/physics/source_base.h
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2026, Liam Pohlmann
+
+#ifndef HUMMINGBIRD_PHYSICS_SOURCE_BASE_H_
+#define HUMMINGBIRD_PHYSICS_SOURCE_BASE_H_
+
+#include "mesh/node.h"
+#include "quadrature/angular/ordinate.h"
+
+namespace hummingbird {
+/**
+ * @brief Base class for independent sources
+ *
+ */
+class SourceBase {
+ public:
+  /**
+   * @brief Construct a new Source Base object
+   *
+   */
+  SourceBase() = default;
+
+  /**
+   * @brief Destroy the Source Base object
+   *
+   */
+  ~SourceBase() = default;
+
+ protected:
+  /**
+   * @brief Evaluate the source at a node in a particular direction
+   *
+   * @param node Node
+   * @param ordinate Ordinate (direction)
+   * @return Volumetric source in n/cm2/s/str
+   */
+  virtual double EvaluateAtNode(const Node& node, const Ordinate& ordinate) = 0;
+};
+}  // namespace hummingbird
+
+#endif  // HUMMINGBIRD_PHYSICS_SOURCE_BASE_H_
