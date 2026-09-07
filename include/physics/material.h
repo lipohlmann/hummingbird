b40ce990e306d8207f055767a29ccdcfc8abf221
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-09-07 16:56:44 -0500

    update header guard

--- include/physics/material.h
+++ include/physics/material.h
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2026, Liam Pohlmann
 
-#ifndef HUMMINGBIRD_MATERIAL_H_
-#define HUMMINGBIRD_MATERIAL_H_
+#ifndef HUMMINGBIRD_PHYSICS_MATERIAL_H_
+#define HUMMINGBIRD_PHYSICS_MATERIAL_H_
 
 namespace hummingbird {
 
@@ -26,4 +26,4 @@ struct Material {
 
 }  // namespace hummingbird
 
-#endif  // HUMMINGBIRD_MATERIAL_H_
+#endif  // HUMMINGBIRD_PHYSICS_MATERIAL_H_
