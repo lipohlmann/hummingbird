21d9d9f21cfaf22fc84957979a1c8b525a3e6c1a
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-09-07 23:26:02 -0500

    fill in material bank with heavy influence from starling

--- include/physics/material_bank.h
+++ include/physics/material_bank.h
@@ -4,8 +4,62 @@
 #ifndef HUMMINGBIRD_PHYSICS_MATERIAL_BANK_H_
 #define HUMMINGBIRD_PHYSICS_MATERIAL_BANK_H_
 
+#include <nlohmann/json.hpp>
+#include <string>
+#include <unordered_map>
+
+#include "physics/material.h"
+
+using nlohmann::json;
+
 namespace hummingbird {
+class MaterialBank {
+ public:
+  /**
+   * @brief Construct a new Material Bank object. Material structs are built
+   * using the json input
+   *
+   * @param json_input JSON object containing input file information
+   */
+  MaterialBank(const json& json_input);
 
-}
+  /**
+   * @brief Get the Material object by ID
+   *
+   * @param id Material ID
+   * @return const Material&
+   */
+  const Material& GetMaterial(const unsigned int id) const {
+    return id_material_map_.at(id);
+  }
 
-#endif // HUMMINGBIRD_PHYSICS_MATERIAL_BANK_H_
+  /**
+   * @brief Get the Material object by name
+   *
+   * @param name Material name
+   * @return const Material&
+   */
+  const Material& GetMaterial(const std::string& name) const {
+    return GetMaterial(name_id_map_.at(name));
+  }
+
+ private:
+  /// @brief Map from material ID to the Material object
+  std::unordered_map<unsigned int, Material> id_material_map_;
+
+  /// @brief Map from the material name to the corresponding ID
+  std::unordered_map<std::string, unsigned int> name_id_map_;
+
+  /**
+   * @brief Builds the material structs and assigns them to the id_material_map_
+   * and name_id_map_ members
+   *
+   * @param json_input JSON object containing input file information
+   */
+  void Build(const json& json_input);
+
+  void CheckMaterials();
+};
+}  // namespace hummingbird
+
+#endif  // HUMMINGBIRD_PHYSICS_MATERIAL_BANK_H_
--- src/physics/material_bank.cc
+++ src/physics/material_bank.cc
@@ -1,2 +1,58 @@
 #include "physics/material_bank.h"
 
+namespace hummingbird {
+void from_json(const json& j, Material& material) {
+  material.scattering_xs = j.at("sigma_s").get<double>();
+  material.total_xs = j.at("sigma_t").get<double>();
+  material.fission_xs = j.at("sigma_f").get<double>();
+  material.nu = j.at("nu").get<double>();
+}
+
+MaterialBank::MaterialBank(const json& input_file_json) {
+  Build(input_file_json);
+  CheckMaterials();
+}
+
+void MaterialBank::Build(const json& input_file_json) {
+  for (const auto& [name, material_json] :
+       input_file_json.at("materials").items()) {
+    Material material = material_json.get<Material>();
+
+    const auto id = material_json.at("id").get<unsigned int>();
+    id_material_map_.emplace(id, std::move(material));
+    name_id_map_.emplace(name, id);
+  }
+}
+
+void MaterialBank::CheckMaterials() {
+  std::vector<Material> materials;
+  materials.reserve(id_material_map_.size());
+  for (const auto& [id, material] : id_material_map_)
+    materials.push_back(material);
+
+  for (const auto& material : materials) {
+    if (material.total_xs < (material.scattering_xs + material.fission_xs))
+      throw std::runtime_error(
+          "Sum of scattering and fission cross sections is greater than the "
+          "total cross section. Please check over material data in input "
+          "file.");
+    if (material.total_xs < 0)
+      throw std::runtime_error(
+          "Negative total cross section passed in material. Please check over "
+          "material data in input file.");
+    if (material.scattering_xs < 0)
+      throw std::runtime_error(
+          "Negative scattering cross section passed in material. Please check "
+          "over material data in input file.");
+    if (material.fission_xs < 0)
+      throw std::runtime_error(
+          "Negative fission cross section passed in material. Please check "
+          "over material data in input file.");
+    if (material.nu < 0)
+      throw std::runtime_error(
+          "Negative fission multiplication (nu) passed in material. Please "
+          "check over material data in input file.");
+  }
+}
+
+}  // namespace hummingbird
