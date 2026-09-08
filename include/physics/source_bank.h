27fccdac0de03613a7149a2b373766ad89513d62
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-09-07 23:56:39 -0500

    fill in SourceBank based on MaterialBank, handles abstraction (hopefully...)

--- include/physics/source_bank.h
+++ include/physics/source_bank.h
@@ -4,8 +4,63 @@
 #ifndef HUMMINGBIRD_PHYSICS_SOURCE_BANK_H_
 #define HUMMINGBIRD_PHYSICS_SOURCE_BANK_H_
 
+#include <memory>
+#include <nlohmann/json.hpp>
+#include <string>
+#include <unordered_map>
+
+#include "physics/constant_volumetric_source.h"
+#include "physics/parsed_volumetric_source.h"
+#include "physics/source_base.h"
+
+using nlohmann::json;
+
 namespace hummingbird {
 
-}
+class SourceBank {
+ public:
+  /**
+   * @brief Construct a new Source Bank object. Source structs are build using
+   * the JSON input file
+   *
+   * @param json_input Input file as json object
+   */
+  SourceBank(const json& json_input);
 
-#endif // HUMMINGBIRD_PHYSICS_SOURCE_BANK_H_
+  /**
+   * @brief Get the Source object by ID
+   *
+   * @param id Source ID
+   * @return const std::unique_ptr<SourceBase>&
+   */
+  const std::unique_ptr<SourceBase>& GetSource(const unsigned int id) const {
+    return id_source_map_.at(id);
+  }
+
+  /**
+   * @brief Get the Source object by name
+   *
+   * @param name Source name
+   * @return const std::unique_ptr<SourceBase>&
+   */
+  const std::unique_ptr<SourceBase>& GetSource(const std::string& name) const {
+    return GetSource(name_id_map_.at(name));
+  }
+
+ private:
+  /// @brief Map from source ID to derived SourceBase object
+  std::unordered_map<unsigned int, std::unique_ptr<SourceBase>> id_source_map_;
+
+  /// @brief Map from source name to corresponding ID
+  std::unordered_map<std::string, unsigned int> name_id_map_;
+
+  /**
+   * @brief Build internal maps based on JSON input
+   *
+   * @param json_input
+   */
+  void Build(const json& json_input);
+};
+}  // namespace hummingbird
+
+#endif  // HUMMINGBIRD_PHYSICS_SOURCE_BANK_H_
--- src/physics/source_bank.cc
+++ src/physics/source_bank.cc
@@ -1,2 +1,22 @@
 #include "physics/source_bank.h"
 
+namespace hummingbird {
+
+void SourceBank::Build(const json& json_input) {
+  unsigned int id = 0;
+  for (const auto& [name, source_json] : json_input.at("sources").items()) {
+    if (source_json.at("type").get<std::string>() == "parsed_function") {
+      ParsedVolumetricSource pvs(
+          source_json.at("expression").get<std::string>());
+      id_source_map_.emplace(id, std::move(pvs));
+      name_id_map_.emplace(name, id);
+      id++;
+    } else if (source_json.at("type").get<std::string>() == "constant") {
+      ConstantVolumetricSource cvs(source_json.at("strength").get<double>());
+      id_source_map_.emplace(id, std::move(cvs));
+      name_id_map_.emplace(name, id);
+      id++;
+    }
+  }
+}
+}  // namespace hummingbird
