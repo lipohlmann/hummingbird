2d9eaf9bcc87a0569b12e57a0e3c871d46ed036b
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-09-11 14:56:45 -0500

    fill in bc_bank using bank_base

--- include/banks/bc_bank.h
+++ include/banks/bc_bank.h
@@ -4,8 +4,23 @@
 #ifndef HUMMINGBIRD_BANKS_BC_BANK_H_
 #define HUMMINGBIRD_BANKS_BC_BANK_H_
 
+#include <nlohmann/json.hpp>
+#include <string>
+#include <unordered_map>
+
+#include "banks/bank_base.h"
+#include "utils/enums.h"
+
+using nlohmann::json;
 namespace hummingbird {
 
-}
+class BCBank : public BankBase<BC> {
+ public:
+  BCBank(const json& json_input);
 
-#endif // HUMMINGBIRD_BANKS_BC_BANK_H_
+ private:
+  void Build(const json& json_input) override;
+};
+}  // namespace hummingbird
+
+#endif  // HUMMINGBIRD_BANKS_BC_BANK_H_
--- src/banks/bc_bank.cc
+++ src/banks/bc_bank.cc
@@ -1,2 +1,16 @@
 #include "banks/bc_bank.h"
 
+namespace hummingbird {
+BCBank::BCBank(const json& json_input) { Build(json_input); }
+
+void BCBank::Build(const json& json_input) {
+  unsigned int id = 0;
+  for (const auto& [name, bc_json] :
+       json_input.at("boundary_conditions").items()) {
+    BC bc = bc_json.at("type");
+    id_object_map_.emplace(id, std::move(bc));
+    name_id_map_.emplace(name, id);
+    id++;
+  }
+}
+}  // namespace hummingbird
