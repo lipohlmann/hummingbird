52c1fa96694e55b62c033373f15cb8a3a433f7f7
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-09-15 21:43:27 -0500

    add virtual methods and a struct

--- include/problem/problem_base.h
+++ include/problem/problem_base.h
@@ -4,8 +4,30 @@
 #ifndef HUMMINGBIRD_PROBLEM_PROBLEM_BASE_H_
 #define HUMMINGBIRD_PROBLEM_PROBLEM_BASE_H_
 
+#include <armadillo>
+#include <array>
+
+#include "mesh/mesh.h"
+
 namespace hummingbird {
+struct GlobalMatrixData {
+  size_t row_id;
+  size_t col_id;
+  double stiffness_contrib;
+  double mass_contrib;
+};
 
-}
+class ProblemBase {
+ public:
+  void AssembleGlobalSystem(const GlobalMatrixData& global_matrix_data);
+  virtual void Solve() = 0;
 
-#endif // HUMMINGBIRD_PROBLEM_PROBLEM_BASE_H_
+ protected:
+  arma::SpMat<double> global_system_matrix_;
+  arma::SpMat<double> global_mass_matrix_;
+  arma::SpMat<double> global_stiffness_matrix_;
+  arma::Col<double> global_forcing_vector_;
+};
+}  // namespace hummingbird
+
+#endif  // HUMMINGBIRD_PROBLEM_PROBLEM_BASE_H_
