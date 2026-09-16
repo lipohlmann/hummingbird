f0f11035789dbc99b60efcf3c0c941c677389441
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-09-16 13:55:08 -0500

    add NONE to first boundary condition

--- src/banks/bc_bank.cc
+++ src/banks/bc_bank.cc
@@ -5,6 +5,10 @@ BCBank::BCBank(const json& json_input) { Build(json_input); }
 
 void BCBank::Build(const json& json_input) {
   unsigned int id = 0;
+  id_object_map_.emplace(id, BC::NONE);
+  name_id_map_.emplace(std::string("none"), id);
+  id++;
+
   for (const auto& [name, bc_json] :
        json_input.at("boundary_conditions").items()) {
     BC bc = bc_json.at("type");
