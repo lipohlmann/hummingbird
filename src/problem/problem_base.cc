22bdb5da84eef8bf7bc341c2c40f536c29239201
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-09-15 22:13:07 -0500

    fill in AssembleGlobalSystem -- something is throwing with the sparse matrix constructor...

--- src/problem/problem_base.cc
+++ src/problem/problem_base.cc
@@ -1,2 +1,17 @@
 #include "problem/problem_base.h"
 
+namespace hummingbird {
+void ProblemBase::AssembleGlobalSystem(
+    const std::vector<GlobalMatrixData>& global_matrix_data) {
+  auto n_vals = global_matrix_data.size();
+  arma::umat locations(2, n_vals);
+  std::vector<double> values(n_vals);
+  for (auto i = 0; i < n_vals; i++) {
+    locations(1, i) = global_matrix_data[i].row_id;
+    locations(2, i) = global_matrix_data[i].col_id;
+    values[i] = global_matrix_data[i].value;
+  }
+  arma::SpMat<double> global_system_matrix(true, locations, values, n_vals,
+                                           n_vals);
+}
+}  // namespace hummingbird
